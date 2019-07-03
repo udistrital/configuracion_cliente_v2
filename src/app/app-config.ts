611aa0fa8f624264a2ec5626dded91025b5b82d1
Oscar Duarte <o.duarte7@gmail.com>
2019-07-03 15:54:26 -0500

    Se cambia token

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -10,13 +10,13 @@ export const Config = {
         NOTIFICACION_SERVICE: 'ws://pruebasapi.intranetoas.udistrital.edu.co:8116/ws/join',
         CONF_MENU_SERVICE: 'http://10.20.0.254/configuracion_api/v1/menu_opcion_padre/ArbolMenus/',
         TOKEN: {
-            AUTORIZATION_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize',
-          CLIENTE_ID: '3Idp5LUlnZY7cOV10NaLuyRfzooa',
+            AUTORIZATION_URL: 'https://autenticacion.udistrital.edu.co/oauth2/authorize',
+          CLIENTE_ID: 'qGicYmef58iY7VxyFm8B39995FUa',
           RESPONSE_TYPE: 'id_token token',
           SCOPE: 'openid email role documento',
-          REDIRECT_URL: 'http://localhost:4200/',
-          SIGN_OUT_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oidc/logout',
-          SIGN_OUT_REDIRECT_URL: 'http://localhost:4200/',
+          REDIRECT_URL: 'http://10.20.0.254/configuracionv2/',
+          SIGN_OUT_URL: 'https://autenticacion.udistrital.edu.co/oidc/logout',
+          SIGN_OUT_REDIRECT_URL: 'http://10.20.0.254/configuracionv2/',
         },
     },
 };
