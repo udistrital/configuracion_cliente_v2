6496c8c47e5b6f77c34baacc231f7a7293bf6943
Fabián Sánchez León <fadarsaleeing@gmail.com>
2021-01-28 22:27:05 -0500

    #49 Ajuste de entorno prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,19 +7,19 @@ export const environment = {
   appname: 'core',
   appMenu: 'CONFIGURACION',
   NUXEO: {
-      PATH: 'https://documental.udistrital.edu.co/nuxeo/',
+    PATH: 'https://documental.udistrital.edu.co/nuxeo/',
   },
   CONFIGURACION_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/',
   CONF_MENU_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/menu_opcion_padre/ArbolMenus/',
   NOTIFICACION_SERVICE: 'wss://pruebasapi.portaloas.udistrital.edu.co:8116/ws',
   TOKEN: {
-      AUTORIZATION_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize',
-      CLIENTE_ID: 'e36v1MPQk2jbz9KM4SmKhk8Cyw0a',
-      RESPONSE_TYPE: 'id_token token',
-      SCOPE: 'openid email',
-      REDIRECT_URL: 'http://localhost:4200/',
-      SIGN_OUT_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oidc/logout',
-      SIGN_OUT_REDIRECT_URL: 'http://localhost:4200/',
-      AUTENTICACION_MID: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/autenticacion_mid/v1/token/userRol',
+    AUTORIZATION_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize',
+    CLIENTE_ID: '1RQxbw6ReM_D3j1nZu8DOkYI1T4a', // RgZGDAPQLhEm1UFwyltcMfaAe74a - google
+    RESPONSE_TYPE: 'id_token token',
+    SCOPE: 'openid email',
+    REDIRECT_URL: 'https://configuracion.portaloas.udistrital.edu.co',
+    SIGN_OUT_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oidc/logout',
+    SIGN_OUT_REDIRECT_URL: 'https://configuracion.portaloas.udistrital.edu.co',
+    AUTENTICACION_MID: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/autenticacion_mid/v1/token/userRol',
   },
 };
